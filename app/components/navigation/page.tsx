4f0a38e3c676ce62b7aa9148bff47e2d34f3c7f6
Tenzing Sherpa <tsherpa2016@gmail.com>
2024-02-26 14:30:29 -0500

    Added linting rules/applied to existing files

--- app/components/navigation/page.tsx
+++ app/components/navigation/page.tsx
@@ -1,5 +1,5 @@
-import Link from 'next/link';
-import Image from 'next/image';
+import Link from "next/link";
+
 export default function Navigation() {
   return (
     <div className="navigation-container flex p-20 text-white justify-between items-center w-full">
@@ -46,7 +46,7 @@ export default function Navigation() {
               strokeLinejoin="round"
               d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25"
             />
-          </svg>{' '}
+          </svg>{" "}
           Home
         </Link>
         <Link className="flex gap-2" href="/pages/portfolio">
@@ -82,7 +82,6 @@ export default function Navigation() {
             d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z"
           />
         </svg>
-
         <input
           className="bg-darkIndigo relative rounded-md p-3 pl-12 w-8/12 text-sm"
           type="text"
@@ -103,7 +102,6 @@ export default function Navigation() {
               d="M12 6v12m-3-2.818.879.659c1.171.879 3.07.879 4.242 0 1.172-.879 1.172-2.303 0-3.182C13.536 12.219 12.768 12 12 12c-.725 0-1.45-.22-2.003-.659-1.106-.879-1.106-2.303 0-3.182s2.9-.879 4.006 0l.415.33M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
             />
           </svg>
-
           <p>USD</p>
           <svg
             xmlns="http://www.w3.org/2000/svg"
