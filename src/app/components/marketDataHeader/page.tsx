19f9b69e74ea62b2a7e8c37bb8806f74f12ed6bb
Tenzing Sherpa <tsherpa2016@gmail.com>
2024-03-21 00:22:20 -0400

    marketData styling

--- src/app/components/marketDataHeader/page.tsx
+++ src/app/components/marketDataHeader/page.tsx
@@ -5,16 +5,18 @@ import { fetchMarketData } from "@/lib/features/marketSlice";
 import { convertNumber, convertToPercent } from "../../../../utils/conversions";
 
 const MarketDataHeader = () => {
-  const dispatch = useAppDispatch();
-  useEffect(() => {
-    dispatch(fetchMarketData());
-  }, []);
+  // const dispatch = useAppDispatch();
+  // useEffect(() => {
+  //   dispatch(fetchMarketData());
+  //   console.log("dispatch");
+  // }, [dispatch]);
 
-  const marketData = useAppSelector((state) => state.marketData.data);
-  const { data } = marketData;
+  // const marketData = useAppSelector((state) => state.marketData.data);
+  // const { data } = marketData;
+  // console.log(data);
   return (
-    <div>
-      {!data ? (
+    <div className="market-data-container flex bg-lightIndigo w-full justify-center items-center py-5 ">
+      {/* {!data ? (
         <p>Loading...</p>
       ) : (
         <>
@@ -25,7 +27,191 @@ const MarketDataHeader = () => {
           <p>{convertToPercent(data.market_cap_percentage.btc)}</p>
           <p>{convertToPercent(data.market_cap_percentage.eth)}</p>
         </>
-      )}
+      )} */}
+      <div className="flex gap-16 w">
+        <div className="flex items-center gap-2">
+          <svg
+            width="14"
+            height="14"
+            viewBox="0 0 14 14"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M7.00065 0.333252C3.32065 0.333252 0.333984 3.31992 0.333984 6.99992C0.333984 10.6799 3.32065 13.6666 7.00065 13.6666C10.6807 13.6666 13.6673 10.6799 13.6673 6.99992C13.6673 3.31992 10.6807 0.333252 7.00065 0.333252ZM9.35398 7.81992L5.90065 10.2133C5.39398 10.5666 5.10065 10.3599 5.24732 9.76659L5.88065 7.20659L4.78065 6.93325C4.28065 6.81325 4.22065 6.46659 4.64065 6.17325L8.09398 3.77992C8.60065 3.42659 8.89398 3.63325 8.74732 4.22659L8.11398 6.78659L9.21398 7.05992C9.71398 7.18659 9.77398 7.52659 9.35398 7.81992Z"
+              fill="white"
+            />
+          </svg>
+          <p className="text-xs font-medium ">Coins 13361</p>
+        </div>
+        <div className="flex items-center gap-2">
+          <svg
+            width="16"
+            height="16"
+            viewBox="0 0 16 16"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M9.99948 15.1667C9.81948 15.1667 9.65281 15.0667 9.56614 14.9134C9.47948 14.7534 9.47948 14.5667 9.57281 14.4067L10.2728 13.2401C10.4128 13.0001 10.7261 12.9267 10.9595 13.0667C11.1995 13.2067 11.2728 13.5134 11.1328 13.7534L10.9528 14.0534C12.7928 13.6201 14.1728 11.9667 14.1728 9.99341C14.1728 9.72007 14.3995 9.49341 14.6728 9.49341C14.9461 9.49341 15.1728 9.72007 15.1728 9.99341C15.1661 12.8467 12.8461 15.1667 9.99948 15.1667Z"
+              fill="white"
+            />
+            <path
+              d="M1.33398 6.49992C1.06065 6.49992 0.833984 6.27325 0.833984 5.99992C0.833984 3.15325 3.15398 0.833252 6.00065 0.833252C6.18065 0.833252 6.34732 0.933252 6.43398 1.08659C6.52065 1.24659 6.52065 1.43325 6.42732 1.59325L5.72732 2.75992C5.58732 2.99325 5.28065 3.07325 5.04065 2.92658C4.80732 2.78659 4.72732 2.47992 4.87398 2.23992L5.05398 1.93992C3.20732 2.37325 1.83398 4.02659 1.83398 5.99992C1.83398 6.27325 1.60732 6.49992 1.33398 6.49992Z"
+              fill="white"
+            />
+            <path
+              d="M9.86732 8.41997V10.38C9.86732 12.0133 9.21398 12.6666 7.58065 12.6666H5.62065C3.98732 12.6666 3.33398 12.0133 3.33398 10.38V8.41997C3.33398 6.78663 3.98732 6.1333 5.62065 6.1333H7.58065C9.21398 6.1333 9.86732 6.78663 9.86732 8.41997Z"
+              fill="white"
+            />
+            <path
+              d="M10.3806 3.33325H8.42063C6.81396 3.33325 6.16062 3.97325 6.14062 5.54659H7.58062C9.54063 5.54659 10.4473 6.45992 10.4473 8.41325V9.85325C12.0273 9.83325 12.6606 9.17992 12.6606 7.57325V5.61992C12.6673 3.98659 12.014 3.33325 10.3806 3.33325Z"
+              fill="white"
+            />
+          </svg>
+          <p className="text-xs font-medium ">Exchange 1005</p>
+        </div>
+        <div className="flex items-center gap-2">
+          <svg
+            width="16"
+            height="16"
+            viewBox="0 0 16 16"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M8.00065 6.33325L4.66732 9.66659H11.334L8.00065 6.33325Z"
+              fill="#01F1E3"
+            />
+          </svg>
+          <p className="text-xs font-medium ">$2.19M</p>
+        </div>
+        <div className="flex items-center gap-2">
+          <p className="text-xs font-medium ">$39.80M</p>
+          <svg
+            width="53"
+            height="6"
+            viewBox="0 0 53 6"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <rect
+              x="2.37109"
+              width="50.6283"
+              height="6"
+              rx="2"
+              fill="white"
+              fillOpacity="0.4"
+            />
+            <rect width="12" height="6" rx="2" fill="white" />
+          </svg>
+        </div>
+        <div className="flex items-center gap-2">
+          <svg
+            width="16"
+            height="16"
+            viewBox="0 0 16 16"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <g clipPath="url(#clip0_422_6902)">
+              <path
+                d="M16 8C16 9.58225 15.5308 11.129 14.6518 12.4446C13.7727 13.7602 12.5233 14.7855 11.0615 15.391C9.59966 15.9965 7.99113 16.155 6.43928 15.8463C4.88743 15.5376 3.46197 14.7757 2.34315 13.6569C1.22433 12.538 0.462403 11.1126 0.153721 9.56072C-0.15496 8.00887 0.00346624 6.40034 0.608967 4.93853C1.21447 3.47672 2.23985 2.22729 3.55544 1.34824C4.87104 0.469192 6.41775 0 8 0C10.1217 0 12.1566 0.842855 13.6569 2.34315C15.1572 3.84344 16 5.87827 16 8Z"
+                fill="#F7931A"
+              />
+              <path
+                fillRule="evenodd"
+                clipRule="evenodd"
+                d="M5.68647 3.65304L7.52501 4.14541L7.93592 2.61377L8.85519 2.86322L8.46029 4.33159L9.2101 4.53304L9.60574 3.04868L10.541 3.29886L10.1381 4.79122C10.1381 4.79122 11.6654 5.12941 12.0246 6.37159C12.3839 7.61377 11.2348 8.26613 10.8799 8.29086C10.8799 8.29086 12.2181 9.02468 11.7585 10.4683C11.2988 11.912 9.88792 12.1701 8.40356 11.8392L8.00065 13.3869L7.06538 13.1367L7.47629 11.613L6.73447 11.4109L6.32356 12.9454L5.39556 12.696L5.80719 11.1687L3.91992 10.6596L4.39556 9.60359C4.39556 9.60359 4.92792 9.74904 5.12938 9.79704C5.33083 9.84504 5.46029 9.63559 5.51701 9.42613C5.57374 9.21668 6.42829 5.74541 6.50901 5.45959C6.58974 5.17377 6.55701 4.9505 6.2181 4.8625C5.87919 4.7745 5.4181 4.63704 5.4181 4.63704L5.68647 3.65304ZM7.54101 8.25013L7.03192 10.2741C7.03192 10.2741 9.55629 11.1854 9.87919 9.90322C10.2021 8.62104 7.54101 8.25013 7.54101 8.25013ZM7.77519 7.29013L8.27483 5.43559C8.27483 5.43559 10.4363 5.8225 10.1701 6.8545C9.90392 7.8865 8.62974 7.49086 7.77519 7.29013Z"
+                fill="white"
+              />
+            </g>
+            <defs>
+              <clipPath id="clip0_422_6902">
+                <rect width="16" height="16" fill="white" />
+              </clipPath>
+            </defs>
+          </svg>
+          <p className="text-xs font-medium ">49%</p>
+          <svg
+            width="53"
+            height="6"
+            viewBox="0 0 53 6"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <rect
+              x="1.89453"
+              width="51.1059"
+              height="6"
+              rx="2"
+              fill="white"
+              fillOpacity="0.4"
+            />
+            <rect width="25" height="6" rx="2" fill="#F7931A" />
+          </svg>
+        </div>
+        <div className="flex items-center gap-2">
+          <svg
+            width="16"
+            height="16"
+            viewBox="0 0 16 16"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <g clipPath="url(#clip0_422_6905)">
+              <circle cx="8" cy="8" r="8" fill="#627EEA" />
+              <path
+                d="M7.9564 2.07959L7.87695 2.34943V10.179L7.9564 10.2583L11.5907 8.10999L7.9564 2.07959Z"
+                fill="#C0CBF6"
+              />
+              <path
+                d="M7.95474 2.07959L4.32031 8.10999L7.95474 10.2583V6.45801V2.07959Z"
+                fill="white"
+              />
+              <path
+                d="M7.95493 10.945L7.91016 10.9996V13.7886L7.95493 13.9193L11.5915 8.79785L7.95493 10.945Z"
+                fill="#C0CBF6"
+              />
+              <path
+                d="M7.95474 13.9193V10.945L4.32031 8.79785L7.95474 13.9193Z"
+                fill="white"
+              />
+              <path
+                d="M7.95508 10.2595L11.5894 8.11121L7.95508 6.45923V10.2595Z"
+                fill="#8197EE"
+              />
+              <path
+                d="M4.32031 8.11121L7.95474 10.2595V6.45923L4.32031 8.11121Z"
+                fill="#C0CBF6"
+              />
+            </g>
+            <defs>
+              <clipPath id="clip0_422_6905">
+                <rect width="16" height="16" fill="white" />
+              </clipPath>
+            </defs>
+          </svg>
+          <p className="text-xs font-medium ">16%</p>
+          <svg
+            width="53"
+            height="6"
+            viewBox="0 0 53 6"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <rect
+              x="1.89453"
+              width="51.1059"
+              height="6"
+              rx="2"
+              fill="white"
+              fillOpacity="0.4"
+            />
+            <rect width="12.7765" height="6" rx="2" fill="#849DFF" />
+          </svg>
+        </div>
+      </div>
     </div>
   );
 };
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,8 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import Navigation from "./components/navigation/page";
 import MarketDataHeader from "./components/marketDataHeader/page";
 import StoreProvider from "./StoreProvider";
 import "./globals.css";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -18,7 +15,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${inter.className}  bg-main`}>
+      <body className=" bg-main">
         <StoreProvider>
           <MarketDataHeader />
           <Navigation />
