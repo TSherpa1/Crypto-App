4675017b978a3b0e1d669bca4db1009a355086c0
Tenzing Sherpa <tsherpa2016@gmail.com>
2024-05-30 19:16:18 -0400

    Renamed currencySelector to coinSelector (had duplicate file/component names)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
 "use client";
-import CurrencySelector from "./components/homeComponents/currencySelector/CurrencySelector";
+import CoinSelector from "./components/homeComponents/coinSelector/CoinSelector";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24 w-11/2">
-      <CurrencySelector />
+      <CoinSelector />
     </main>
   );
 }
